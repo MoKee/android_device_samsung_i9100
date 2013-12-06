278a64362eeb5113f07c470706721cc62f11d638
Ryuinferno <ryuinferno.xda@gmail.com>
2013-12-06 15:26:37 +0100

    i9100: Force system dalvik files to be generated in /data
    The /cache partition of i9100 is not sufficient to hold both .dex and .oat files when ART runtime is used
    
    Change-Id: Iaf0ee3a41dd8e01a312bc50dd986178da9539793

--- i9100.mk
+++ i9100.mk
@@ -28,7 +28,8 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_LOCALES += hdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240
+    ro.sf.lcd_density=240 \
+    dalvik.vm.dexopt-data-only=1
 
 # Sensors
 PRODUCT_PACKAGES += \
